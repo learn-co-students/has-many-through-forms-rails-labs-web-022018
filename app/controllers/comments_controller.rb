45f877b3a43dac5a7ba763e56b57ca2cad6abe1d
github username <github email address>
2018-03-21 17:00:46 -0400

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,15 @@
 class CommentsController < ApplicationController
 
   def create
+    byebug
     comment = Comment.create(comment_params)
-    redirect_to comment.post
+    post = Post.find(request.referrer.split("/").last)
+    comment.post = post
+    redirect_to post_path(post)
   end
 
-  private
-
+#   private
+#
   def comment_params
     params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
   end
 
   def index
@@ -11,14 +12,38 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def update
+    @post = Post.find(params[:id])
+    if params["post"]["users_attributes"]["0"]["username"].empty?
+      user = User.find_or_create_by(id: params["post"]["users"])
+      @comment = Comment.create(content: params["post"]["comments_attributes"]["0"]["content"], user_id: user.id)
+    else
+      user = User.find_or_create_by(username: params["post"]["users_attributes"]["0"]["username"])
+      @comment = Comment.create(content: params["post"]["comments_attributes"]["0"]["content"], user_id: user.id)
+    end
+
+    @post.comments << @comment
+    # if @post.update(post_params)
+    #   redirect_to post_path(@post)
+    # else
+    #   render :show
+    # end
+    redirect_to post_path(@post)
+  end
+
   def create
-    post = Post.create(post_params)
-    redirect_to post
+    post = Post.new(post_params)
+
+    if post.save
+      redirect_to post
+    else
+      render :new
+    end
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name], user_attributes: [:username], comments_attributes: [:content])
   end
 end
